58da5020e08a774f4d0a97d5887de17d200cf7a6
matborba2 <110839515+matheusborbaa@users.noreply.github.com>
2025-02-05 21:07:29 -0300

    Commit Inicial

---
+++ header.php
@@ -0,0 +1,24 @@
+
+   <!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width">
+    <?php wp_head(); ?></head>
+
+    <body <?php body_class(); ?>>
+    <header>
+        <nav>
+            <img class="logo" src="assets/Logo__3.png" alt="">
+            <div class="nav-links">
+                <a href="#home">Home</a>
+                <a href="#beleza">Beleza</a>
+                <a href="#estetica">Estética</a>
+                <a href="#contato">Contato</a>
+            </div>
+        </nav>
+        <div class="progress-bar" ></div>
+    </header>
---
+++ templates/template-inicio.php
@@ -0,0 +1,202 @@
+
+    <?php
+
+/**
+ * Template Name: Início
+ */
+
+get_header();
+?>
+    <section class="hero" id="home">
+        
+    </section>
+
+    <section class="services" id="beleza">
+        <h2>Nossos serviços</h2>
+        <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis ex nihil dolore non
+            doloremque harum.</p>
+
+        <div class="services-carousel"> 
+            <div class="service-card">
+                <div class="service-image"></div>
+                <h3>Beauty Care</h3>
+                <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
+            </div>
+            <div class="service-card">
+                <div class="service-image"></div>
+                <h3>Body Care</h3>
+                <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
+            </div>
+            <div class="service-card">
+                <div class="service-image"></div>
+                <h3>Face Care</h3>
+                <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
+            </div>
+            <div class="service-card">
+                <div class="service-image"></div>
+                <h3>Hair Care</h3>
+                <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
+            </div>
+            <div class="service-card">
+                <div class="service-image"></div>
+                <h3>Hair Care</h3>
+                <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
+            </div>
+            <div class="service-card">
+                <div class="service-image"></div>
+                <h3>Hair Care</h3>
+                <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
+            </div>
+            <div class="service-card">
+                <div class="service-image"></div>
+                <h3>Hair Care</h3>
+                <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
+            </div>
+        </div>
+        <div class="scroll-indicators">
+            <span class="indicator active"></span>
+            <span class="indicator"></span>
+            <span class="indicator"></span>
+        </div>
+    </section>
+
+    <section class="testimonials">
+        <div class="testimonials-carousel">
+            <div class="testimonials-grid">
+                <!-- Primeiro par de cards -->
+                <div class="testimonial-card">
+                    <div class="testimonial-image">
+                        <img src="assets/mulher3.jpg" alt="Teresa Sobral" />
+                    </div>
+                    <div class="testimonial-content">
+                        <p class="testimonial-quote">
+                            Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.
+                        </p>
+                        <h3 class="testimonial-author">Teresa Sobral</h3>
+                        <p class="testimonial-role">/ Advogada</p>
+                    </div>
+                </div>
+    
+                <div class="testimonial-card">
+                    <div class="testimonial-image">
+                        <img src="assets/mulher2.jpg" alt="Amanda Batista" />
+                    </div>
+                    <div class="testimonial-content">
+                        <p class="testimonial-quote">
+                            Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.
+                        </p>
+                        <h3 class="testimonial-author">Amanda Batista</h3>
+                        <p class="testimonial-role">/ Engenheira</p>
+                    </div>
+                </div>
+                
+    
+                <!-- Segundo par de cards -->
+                <div class="testimonial-card">
+                    <div class="testimonial-image">
+                        <img src="assets/mulher1.jpg" alt="Carla Lopez" />
+                    </div>
+                    <div class="testimonial-content">
+                        <p class="testimonial-quote">
+                            Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.
+                        </p>
+                        <h3 class="testimonial-author">Carla Lopez</h3>
+                        <p class="testimonial-role">/ Professora</p>
+                    </div>
+                </div>
+    
+                <div class="testimonial-card">
+                    <div class="testimonial-image">
+                        <img src="assets/image 10.png" alt="Jennifer" />
+                    </div>
+                    <div class="testimonial-content">
+                        <p class="testimonial-quote">
+                            Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.
+                        </p>
+                        <h3 class="testimonial-author">Jennifer</h3>
+                        <p class="testimonial-role">/ Lojista</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="scroll-indicators">
+            <span class="indicator active"></span>
+            <span class="indicator"></span>
+            <span class="indicator"></span>
+        </div>
+    </section>
+
+
+    <!-- 
+    <section class="testimonials" id="estetica">
+        <div class="testimonials-carousel">
+            <div class="testimonial-card">
+                <img class="testimonial-image" src="" alt="">
+                <div class="testimonial-right">
+                    <h3>Beauty Care</h3>
+                    <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
+                    <p class="author">Teresa Sobral</p>
+                    <p class="role">/ Advogada</p>
+                </div>
+            </div>
+            <div class="testimonial-card">
+                <img class="testimonial-image" src="" alt="">
+                <div class="testimonial-right">
+                    <h3>Beauty Care</h3>
+                    <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
+                    <p class="author">Amanda Batista</p>
+                    <p class="role">/ Engenheira</p>
+                </div>
+
+            </div>
+            <div class="testimonial-card">
+                <img class="testimonial-image" src="" alt="">
+                <div class="testimonial-right">
+                    <h3>Beauty Care</h3>
+                    <p>Lorem ipsum dolor sit amet. In quaerat rerum est minus asperiores ut rerum perspiciatis.</p>
+                    <p class="author">Carla Lopez</p>
+                    <p class="role">/ Professora</p>
+                </div>
+
+            </div>
+        </div>
+        <div class="scroll-indicators">
+            <span class="indicator active"></span>
+            <span class="indicator"></span>
+            <span class="indicator"></span>
+        </div>
+    </section>-->
+
+    <footer id="contato">
+        <div class="footer-content">
+            <img src="assets/Logo__13 cópia 2.png" alt="">
+            <div class="informations">
+                <div>
+                    <p>Endereço: 133/2 - Lisboa - PT</p>
+                    <p>www.bonadea.pt</p>
+                </div>
+                <div>
+                    <p>contato@bonadea.pt</p>
+                    <p>91264-5395</p>
+                </div>
+                <div>
+                    <p>De Terça a Sábado das 9h às 19h</p>
+                    <p>Mais de 160 MIL Mulheres transformadas</p>
+                </div>
+            </div>
+            <p>www.bonadea.pt. All Right Reserved</p>
+        </div>
+    </footer>
+
+    <script>
+        // Progress bar
+        window.addEventListener('scroll', () => {
+            const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+            const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+            const scrolled = (winScroll / height) * 100;
+            document.querySelector('.progress-bar').style.width = scrolled + '%';
+        });
+    </script>
+</body>
+
+</html>
