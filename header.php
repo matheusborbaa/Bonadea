b1939d53374520c3e8013a946cd7bb6ca92d147d
uemanuelb <uemanuelbsilva@gmail.com>
2025-02-09 16:14:12 -0300

    emanuel

--- header.php
+++ header.php
@@ -7,7 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width">
-    <?php wp_head(); ?></head>
+    <?php wp_head(); ?>
+</head>
 
     <body <?php body_class(); ?>>
             <img class="elemento-topo"src="<?php echo _URLTHEME; ?>/assets/img/elemento-topoi.png">
